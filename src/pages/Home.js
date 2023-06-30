2b61b8cf3a3b45169bbfb04d59f65787dc21d6f0
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-06-30 08:05:25 -0500

    more styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div className=" max-w-screen h-full ml-2 mt-2 rounded-m test2">
-            Home
+        <div className=" absolute w-11/12 h-3/4 ml-2 mt-2 rounded-m test2">
+            
         </div>
     )
 }
