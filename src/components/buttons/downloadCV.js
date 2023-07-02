db088e928cb7bd3a413b9f67d3907cde8a778bf7
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-02 12:42:05 -0500

    layout full mobile responsive

--- src/components/buttons/downloadCV.js
+++ src/components/buttons/downloadCV.js
@@ -1,6 +1,6 @@
 export default function DownloadCV() {
     return (
-        <button className="gradient-background font-extralight drop-shadow-xl text-themeWhite border-themeWhite border-opacity-40 border p-2 rounded-lg  hover:border-opacity-90 ">
+        <button className="gradient-background font-extralight drop-shadow-xl text-themeWhite border-themeBlack border-opacity-40 border p-2 rounded-lg hover:border-themeWhite  hover:border-opacity-90 ">
             Download CV
         </button>
     )
