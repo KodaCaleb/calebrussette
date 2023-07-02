5503af93878800b620df7b4d9c0ab7dc1d745d14
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-02 14:59:53 -0500

    project page

---
+++ src/components/assets/projectData.js
@@ -0,0 +1,8 @@
+export const projectData = [
+    {
+        id: 1,
+        title: "Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati distinctio tenetur doloribus numquam vero, fugit atque veniam dolores consequuntur maxime omnis, cumque, similique ipsum odio eius hic exercitationem tempore adipisci.",
+        image: "https://picsum.photos/200/300",
+        link: "https://www.google.com"
+    },
