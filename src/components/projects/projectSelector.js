cd1a88206f5d4ee3c03803b1a64ce0b86c336d02
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-03 13:25:28 -0500

    added conditional rendering for null value on project page

--- src/components/projects/projectSelector.js
+++ src/components/projects/projectSelector.js
@@ -1,46 +1,45 @@
 import { useState } from "react";
 import { projectData } from "../assets/projectData.js";
-
-
+import ProjectCard from "./projectCard.js";
+import { TypeAnimation } from "react-type-animation";
 
 export default function ProjectSelector() {
+  const [currentProject, setCurrentProject] = useState(null);
 
-    const [currentProject, setCurrentProject] = useState(null);
-
-    const handleSetCurrentProject = (projectId) => {
-        const project = projectData.find((data) => data.id === projectId);
-        setCurrentProject(project);
-    };
-
-
+  const handleSetCurrentProject = (projectId) => {
+    const project = projectData.find((data) => data.id === projectId);
+    setCurrentProject(project);
+  };
 
   return (
     <div className=" justify-center p-4 flex  w-full h-full">
       <div className=" justify-center flex h-full m-1 w-1/2">
-        <div className="  ml-2 text-themeWhite self-center justify-center flex-col flex h-4/5 w-full">
-            <div className="h-2/5 flex flex-col ml-14 justify-evenly">
-                
-                <h1 className="text-3xl">
-                    {currentProject?.title}
-                </h1>
-                <h2 className="">
-                    {currentProject?.description}
-                </h2>
+        {currentProject ? (
+          <ProjectCard project={currentProject} />
+        ) : (
+          <div className="self-center justify-center flex-row flex h-1/2 w-full">
+            <div className="h-2/5 w-2/3 flex text-start text-themeWhite text-5xl font-thin flex-col justify-center">
+                <TypeAnimation
+                sequence={["Here's what i've been working on"]}
+                speed={50}
+                />
             </div>
-        </div>
+          </div>
+        )}
       </div>
       <div className="justify-center flex h-full m-1 w-1/2">
         <div className="flex w-1/2 ">
           <div className="flex self-center justify-evenly text-themeWhite flex-col w-full h-2/3">
-            { projectData.map((data) => {
-                return ( 
-                    <button 
-                    onClick={() => handleSetCurrentProject(data.id)}
-                    key={data.id}
-                    className="self-end">
-                        {data.title}
-                    </button>
-                )
+            {projectData.map((data) => {
+              return (
+                <button
+                  onClick={() => handleSetCurrentProject(data.id)}
+                  key={data.id}
+                  className="self-end font-thin"
+                >
+                  {data.title}
+                </button>
+              );
             })}
           </div>
         </div>
