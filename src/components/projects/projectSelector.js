6cefd8df32c2dba32e9732d25b30e063d052652f
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-03 12:24:36 -0500

    added useState for project conditional rendering

--- src/components/projects/projectSelector.js
+++ src/components/projects/projectSelector.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { projectData } from "../assets/projectData.js";
 
 
 
 export default function ProjectSelector() {
+
+    const [currentProject, setCurrentProject] = useState(null);
+
+    const handleSetCurrentProject = (projectId) => {
+        const project = projectData.find((data) => data.id === projectId);
+        setCurrentProject(project);
+    };
+
+
+
   return (
     <div className=" justify-center p-4 flex  w-full h-full">
       <div className=" justify-center flex h-full m-1 w-1/2">
         <div className="  ml-2 text-themeWhite self-center justify-center flex-col flex h-4/5 w-full">
             <div className="h-2/5 flex flex-col ml-14 justify-evenly">
+                
                 <h1 className="text-3xl">
-                    Lorem ipsum dolor sit amet 
+                    {currentProject?.title}
                 </h1>
                 <h2 className="">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati distinctio tenetur doloribus numquam vero, fugit atque veniam dolores consequuntur maxime omnis, cumque, similique ipsum odio eius hic exercitationem tempore adipisci.
+                    {currentProject?.description}
                 </h2>
             </div>
         </div>
@@ -20,9 +32,12 @@ export default function ProjectSelector() {
       <div className="justify-center flex h-full m-1 w-1/2">
         <div className="flex w-1/2 ">
           <div className="flex self-center justify-evenly text-themeWhite flex-col w-full h-2/3">
-            { projectData.map((data, key) => {
+            { projectData.map((data) => {
                 return ( 
-                    <button key={key} className="self-end">
+                    <button 
+                    onClick={() => handleSetCurrentProject(data.id)}
+                    key={data.id}
+                    className="self-end">
                         {data.title}
                     </button>
                 )
@@ -31,7 +46,5 @@ export default function ProjectSelector() {
         </div>
       </div>
     </div>
-
-
   );
 }
