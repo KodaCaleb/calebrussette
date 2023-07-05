7ed5106f32daf085a39c45210861e4274d7fcad3
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-05 10:51:35 -0500

    fixed bug with social bar

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,13 +1,14 @@
 import { Link, useLocation } from "react-router-dom";
+import Social from "./social";
 
 export default function Nav() {
   const location = useLocation();
 
   return (
-    <div className="flex-col">
+    <div className="flex-col ">
       <div className="flex justify-end mb-1 mr-4 sm:ml-8 ml-4 h-16 border border-themeBlack border-opacity-50 rounded-bl-3xl rounded-tr-3xl mt-4 drop-shadow-xl gradient-background">
-        <div className="flex justify-evenly w-full sm:w-3/5  sm:font-light  text-themeWhite text-opacity-80 sm:text-themeBlack sm:text-opacity-100 mr-3">
-          <div className="flex self-center justify-center">
+        <div className=" flex justify-evenly w-full sm:w-3/5  sm:font-light  text-themeWhite text-opacity-80 sm:text-themeBlack sm:text-opacity-100 mr-3">
+        <div className="flex self-center justify-center">
             <Link to={"/"} className={location.pathname === "/" ? "text-themeWhite" : ""}>
               <h1 className="hover:text-themeWhite">Home</h1>
             </Link>
@@ -28,6 +29,10 @@ export default function Nav() {
             </Link>
           </div>
         </div>
+      </div>
+      <div className="flex justify-evenly">
+        <div className="  z-20 gradient-background hidden sm:flex border self-center border-themeBlack border-opacity-40 drop-shadow-lg rounded-tl-xl ml-1 w-4/5 h-4"></div>
+        <Social />
       </div>
     </div>
   );
