b549ccbb44227ee76acd85aacb6d1c052315e3db
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-05 12:49:32 -0500

    fixed headshot mobile responsiveness

--- src/components/about/headshot.js
+++ src/components/about/headshot.js
@@ -1,11 +1,16 @@
+import { motion } from "framer-motion";
+
 export default function Headshot() {
   return (
-    <div className=" h-full w-full flex flex-row justify-start ">
-      <div className=" h-auto w-full flex flex-col justify-center items-center p-10 ">
-        <img
+    <div className="h-full w-full flex flex-row justify-start">
+      <div className="sm:h-2/3 sm:w-full h-full w-full flex flex-col justify-start items-center p-10 sm:pl-20">
+        <motion.img
           src="./Avatar.jpg"
-          className=" w-96 border-b border-themePurple flex rounded-3xl drop-shadow-xl h-auto"
+          className="max-w-full max-h-full border border-themeBlack flex rounded-3xl drop-shadow-2xl"
           alt=""
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
         />
       </div>
     </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import Headshot from "../components/about/headshot";
 import SkillsBioSwitcher from "../components/about/skillsBioSwitcher";
 
-export default function About () {
-    return (
-        <div className="h-full w-full flex-col sm:flex-row flex">
-            <Headshot />
-            <SkillsBioSwitcher />
-        </div>
-    )
+export default function About() {
+  return (
+    <div className="h-full w-full flex-col sm:flex-row flex">
+      <div className="flex-shrink-0 max-w-lg">
+        <Headshot />
+      </div>
+      <SkillsBioSwitcher />
+    </div>
+  );
 }
