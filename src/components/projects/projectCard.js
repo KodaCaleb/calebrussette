512ad3dedf4bc17ce2c2a5a70ad7b703860978ac
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-04 20:27:17 -0500

    project page fully responsive

--- src/components/projects/projectCard.js
+++ src/components/projects/projectCard.js
@@ -4,7 +4,7 @@
 export default function ProjectCard({ project }) {
     const currentProject = project;
     return (
-        <div className="  ml-2 text-themeWhite card-background max-h-full border-themeBlack rounded-xl self-center justify-center flex-col flex h-auto  sm:w-auto">
+        <div className=" md:ml-2 text-themeWhite card-background max-h-full border-themeBlack rounded-xl self-center justify-center flex-col flex h-auto md:w-auto ">
           <div className=" h-auto m-2 rounded-xl drop-shadow-2xl max-h-fit flex w-auto bg-themeBlack self-center flex-col justify-evenly">
             <h1 className="text-3xl m-8">{currentProject?.title}</h1>
             <h2 className="m-8 font-extralight">{currentProject?.description}</h2>
