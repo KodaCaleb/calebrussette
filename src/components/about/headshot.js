97ac506d7a598aeacd126459f5c3eafda4ce8f1d
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-10 19:43:59 -0500

    fixed headshot not showing up on prod

--- src/components/about/headshot.js
+++ src/components/about/headshot.js
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
+import MyHeadshot from "../assets/Avatar.JPG";
 
 export default function Headshot() {
   return (
     <div className="h-full w-full flex flex-row justify-start">
       <div className="sm:h-2/3 sm:w-full h-full w-full flex flex-col justify-start items-center p-10 sm:pl-20">
         <motion.img
-          src="./Avatar.jpg"
+          src={MyHeadshot}
           className="max-w-full max-h-full border border-themeBlack flex rounded-3xl drop-shadow-2xl"
           alt=""
           initial={{ opacity: 0 }}
