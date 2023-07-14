b15f046cc3c3487f2a8ca9355f0664ac9c8a3bc1
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-14 16:34:27 -0500

    further styling skills section

--- src/components/about/skills.js
+++ src/components/about/skills.js
@@ -7,15 +7,14 @@ export default function Skills() {
         <div className=" h-3/4 flex w-3/4 justify-center ">
         <div className="flex flex-col items-center">
           <h1 className="text-themeWhite text-3xl">Skills</h1>
-          <div className="text-themeGrey flex-row mt-4 text-6xl flex h-full">
+          <div className="text-themeGrey flex-row flex-wrap justify-evenly mb-8 mt-4 text-6xl flex h-full">
             {SkillsData.map((skill) => (
             <div 
                 key={skill.id}
-                className="flex flex-col items-center justify-center "
+                className="flex flex-col flex-wrap h-24 items-center justify-center "
                 >
                 {skill.icon}
-                <h1 className="text-sm">{skill.name}</h1>
-                {/* skills data goes here */}
+                <h1 className="text-sm ">{skill.name}</h1>
             </div>
             ))}
           </div>
--- src/components/assets/skillsData.js
+++ src/components/assets/skillsData.js
@@ -1,5 +1,6 @@
-import { BiLogoJavascript } from "react-icons/bi";
-import { FaReact } from "react-icons/fa";
+import { BiLogoJavascript, BiLogoMongodb, BiLogoTailwindCss } from "react-icons/bi";
+import { FaCss3, FaGitAlt, FaGithub, FaHtml5, FaNode, FaReact } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
 
 
 export const SkillsData = [
@@ -10,7 +11,48 @@ export const SkillsData = [
     },
     {
         id: 2,
+        name: "HTML5",
+        icon: <FaHtml5 className="mr-3 ml-3" />,
+
+    },
+    {
+        id: 3,
+        name: "CSS3",
+        icon: <FaCss3 className="mr-3 ml-3" />,
+    },
+    {
+        id: 4,
         name: "React",
         icon: <FaReact className="mr-3 ml-3" />,
     },
+    {
+        id: 6,
+        name: "TailwindCSS",
+        icon: <BiLogoTailwindCss className="mr-3 ml-3" />,
+    },
+    {
+        id: 5,
+        name: "Nodejs",
+        icon: <FaNode className="mr-3 ml-3" />,
+    },
+    {
+        id: 7,
+        name: "Express",
+        icon: <SiExpress className="mr-3 ml-3" />,
+    },
+    {
+        id: 8,
+        name: "MongoDB",
+        icon: <BiLogoMongodb className="mr-3 ml-3" />,
+    },
+    {
+        id: 9,
+        name: "Git",
+        icon: <FaGitAlt className="mr-3 ml-3" />,
+    },
+    {
+        id: 10,
+        
+    }
+
 ]
