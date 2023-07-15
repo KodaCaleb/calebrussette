e659068f76c044b7b79ecf43ede7f4b98cb514b5
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-15 18:57:45 -0500

    added animated text to contact page

---
+++ src/components/contact/contactForm.js
@@ -0,0 +1,7 @@
+export default function ContactForm () {
+    return (
+        <div className="sm:w-1/2 w-full h-full border-themeWhite border">
+
+        </div>
+    )
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,19 @@
+import { TypeAnimation } from "react-type-animation";
+import ContactForm from "../components/contact/contactForm";
+
 export default function Contact() {
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full flex flex-col sm:flex-row">
+            <div className="sm:w-1/2 w-full h-full flex">
+                <div className="self-center w-full h-auto flex justify-center items-start text-3xl sm:text-5xl p-10 sm:p-20 text-themeWhite font-extralight ">
+                    <TypeAnimation
+                    sequence={["Let's build something great together!"]}
+                    speed={50}
+                     />
 
+                </div>
+            </div>
+            <ContactForm />
         </div>
     )
 }
