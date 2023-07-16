1aeb9f441ebbcca8e16679d14e724eed86f0deb3
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-15 19:15:52 -0500

    prepped styling of contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,10 +4,10 @@ import ContactForm from "../components/contact/contactForm";
 export default function Contact() {
     return (
         <div className="w-full h-full flex flex-col sm:flex-row">
-            <div className="sm:w-1/2 w-full h-full flex">
-                <div className="self-center w-full h-auto flex justify-center items-start text-3xl sm:text-5xl p-10 sm:p-20 text-themeWhite font-extralight ">
+            <div className="sm:w-1/2 w-2/3 self-center h-auto  flex">
+                <div className="sm:self-center self-start w-full h-auto text-start sm:h-auto flex justify-center items-start text-3xl sm:text-5xl p-10 sm:p-20 text-themeWhite font-extralight ">
                     <TypeAnimation
-                    sequence={["Let's build something great together!"]}
+                    sequence={["Let's build something great together."]}
                     speed={50}
                      />
 
