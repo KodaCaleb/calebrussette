b11cf2c120a51d9f25d555942fdd3a19b58cd1ac
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-28 19:14:21 -0500

    added bio section text, cleaned up animations, changed project data order

--- src/components/about/bioSection.js
+++ src/components/about/bioSection.js
@@ -1,12 +1,34 @@
 export default function Bio() {
   return (
-    <div className=" h-3/4 flex w-3/4 justify-center flex-shrink-0 ">
+    <div className="h-3/4 flex w-2/3 justify-center flex-shrink-0">
       <div className="flex flex-col items-center">
         <h1 className="text-themeWhite text-3xl">Bio</h1>
-        <h2 className="text-themeWhite mt-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis maiores sequi veritatis quibusdam corporis nobis natus deleniti, ratione dolor qui ad omnis labore enim fuga molestiae accusantium mollitia, laboriosam id! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum tempore obcaecati, itaque et reprehenderit aliquid est ipsum. Perferendis, hic repellat delectus obcaecati fugiat asperiores voluptates, in voluptas incidunt ratione odio?
-        </h2>
+        <p className="text-themeWhite text-xl border-t font-extralight text-center mt-4 overflow-y-auto max-h-80 no-scrollbar  p-4 spacing">
+          Hello, I'm Caleb, a zealous web developer with a lifelong fascination
+          with technology. Ever since I assembled my first computer at 14, I've
+          been weaving intricate narratives through code, pouring hours each day
+          into perfecting my craft.<br /><br />
+          
+          I approach each project with an eagerness to
+          learn, viewing every line of code as an opportunity to tell a story,
+          connecting logic with creativity. My writing passion extends to my
+          work, bringing originality to everything from a simple UI tweak to
+          complex full-stack applications.<br /><br />
+          
+          Whether I'm exploring full-stack,
+          front-end, or back-end roles, I see each challenge as a stepping
+          stone, a chance to grow and fine-tune my skill set. My approach to web
+          development mirrors my love for nonfiction and storytelling:
+          structured, meticulously organized, and always keeping the end-user in
+          mind.<br /><br />
+          
+          It's not just about writing code; it's about creating
+          user-friendly web experiences that resonate. I look forward to
+          embarking on this evolving coding journey with you, ready to learn,
+          grow, and create something truly remarkable.
+        </p>
       </div>
     </div>
   );
 }
+
--- src/components/assets/projectData.js
+++ src/components/assets/projectData.js
@@ -1,56 +1,65 @@
 export const projectData = [
-    {
-        id: 1,
-        title: "Valiant Health",
-        description: "I was contracted to develop a 'coming soon' page for a local health company's website, collaborating closely with the client. This initial success led to an opportunity to join a skilled team in building the complete website. Working alongside talented developers and designers, I'm currently utilizing innovative technologies and following agile methodologies to develop the full website. It has been an exciting professional project, and I look forward to providing updates soon.",
-        image: "./portfolio.png",
-        link: "https://www.valianthealth.org",
-        github: "https://github.com/KodaCaleb/val",
-        technologies: "Nextjs, React, TailwindCSS, Vercel, Figma, MongoDB, Express, Nodejs"
-    }, 
-    {
-        id: 2,
-        title: "ShortStack",
-        description: "This was an ambitious project that me and a small team worked together to create. The objective for this project was to learn more relevent tools and technologies that are used in the industry and how to tackle issues that developers face when providing short form content on a large scale. We faced many challenges during this project, such as CDN's, blob storage, and manipulating video that was being provided from the user. We were able to overcome these challenges and create a fully functional website that allows users to upload short form videos and view other users videos.",
-        image: "./portfolio.png",
-        link: "https://github.com/KodaCaleb",
-        github: "https://github.com/KodaCaleb",
-        technologies: "React, React Native, Nodejs, Firebase, Express, Figma "
-    },
-    {
-        id: 3,
-        title: "KodaCaleb.dev",
-        description: "This website was built to showcase my skills as a developer. I wanted to create a website that was simple, yet elegant. I also wanted to create a website that was easy to navigate and responsive. I used React and TailwindCSS to build this website. I also used Figma to create the design and layout of the website. I used Vercel to deploy the website. I am very happy with the outcome of this website and I am excited to continue to add to it as I learn new technologies and skills.",
-        image: "./portfolio.png",
-        link: "https://www.kodacaleb.dev",
-        github: "https://github.com/KodaCaleb/calebrussette",
-        technologies: "React, TailwindCSS, Nodejs, Express, React Transition Group, Vercel, Figma"
-    },
-    {
-        id: 4,
-        title: "Book n Borrow",
-        description: "Book'n Borrow is a website designed to give users the option to loan out old college textbooks that they are no longer using and to make money by loaning the book. It also allows a user to be able to search for and be able to rent out textbooks that they may need for their courses. Working with a team of six, I was responsible for building the interactivity of this website. This included building out the buttons, front-end routes, and user input database manipulation. It was very exciting working with a team, and confronting all of the challenges that come with it.",
-        image: "./portfolio.png",
-        link: "https://booknborrow.herokuapp.com/",
-        github: "https://github.com/joaqsala/book-n-borrow",
-        technologies: "Handlebars, Bootstrap, Nodejs, Express, MySQL, Sequelize, Heroku, Figma, Jira"
-    },
-    {
-        id: 5,
-        title: "Ecommerce Backend",
-        description: "This project was built to showcase my skills in building a backend for an ecommerce website. I used MySQL and Sequelize to build the database and models. I used Express to build the routes and Nodejs to run the server. I used Postman to test the routes and make sure that they were working properly. I also used Dotenv to hide my sensitive information. It is very exciting to learn the inner workings of a relational database, and how these skills could be used on a much larger scale.",
-        image: "./portfolio.png",
-        link: "Non-deployable",
-        github: "https://github.com/KodaCaleb/ecommerce",
-        technologies: "Nodejs, Express, MySQL, Sequelize, Dotenv, Postman"
-    },
-    {
-        id: 6,
-        title: "Book Search Engine",
-        description: "This project is emblematic of the fact that most modern websites are driven by two things: data, and user demands. The ability to personalize user data is the cornerstone of real-world web development, and I took on this project to learn how to deal with this challenge. While building this application I was faced with the challenges that come with manipulating data within a non-relational database (mongoDB using GraphQL), and learned how to overcome many of these challenges. I have found that I absolutely love using GraphQL and Apollo Server and am very excited to use these again in the future.",
-        image: "./portfolio.png",
-        link: "https://github.com/KodaCaleb",
-        github: "https://github.com/KodaCaleb",
-        technologies: "React, GraphQL, Apollo Server, MongoDB, Heroku"
-    },    
-]
+  {
+    id: 2,
+    title: "ShortStack",
+    description:
+      "This was an ambitious project that me and a small team worked together to create. The objective for this project was to learn more relevent tools and technologies that are used in the industry and how to tackle issues that developers face when providing short form content on a large scale. We faced many challenges during this project, such as CDN's, blob storage, and manipulating video that was being provided from the user. We were able to overcome these challenges and create a fully functional website that allows users to upload short form videos and view other users videos.",
+    image: "./portfolio.png",
+    link: "https://shortstack.vercel.app/",
+    github: "https://github.com/KodaCaleb/ShortStack",
+    technologies: "React, TailwindCSS, Google Cloud Platform, Nodejs, Firebase, Express, Figma ",
+  },
+  {
+    id: 1,
+    title: "Valiant Health",
+    description:
+      "I was contracted to develop a 'coming soon' page for a local health company's website, collaborating closely with the client. This initial success led to an opportunity to join a skilled team in building the complete website. Working alongside talented developers and designers, I'm currently utilizing innovative technologies and following agile methodologies to develop the full website. It has been an exciting professional project, and I look forward to providing updates soon.",
+    image: "./portfolio.png",
+    link: "https://www.valianthealth.org",
+    github: "https://github.com/KodaCaleb/val",
+    technologies:
+      "Nextjs, React, TailwindCSS, Vercel, Figma, MongoDB, Express, Nodejs, AWS",
+  },
+  {
+    id: 3,
+    title: "KodaCaleb.dev",
+    description:
+      "This website was built to showcase my skills as a developer. I wanted to create a website that was simple, yet elegant. I also wanted to create a website that was easy to navigate and responsive. I used React and TailwindCSS to build this website. I also used Figma to create the design and layout of the website. I used Vercel to deploy the website. I am very happy with the outcome of this website and I am excited to continue to add to it as I learn new technologies and skills.",
+    image: "./portfolio.png",
+    link: "https://www.kodacaleb.dev",
+    github: "https://github.com/KodaCaleb/calebrussette",
+    technologies:
+      "React, TailwindCSS, Nodejs, React Transition Group, Vercel, Figma",
+  },
+  {
+    id: 4,
+    title: "Book n Borrow",
+    description:
+      "Book'n Borrow is a website designed to give users the option to loan out old college textbooks that they are no longer using and to make money by loaning the book. It also allows a user to be able to search for and be able to rent out textbooks that they may need for their courses. Working with a team of six, I was responsible for building the interactivity of this website. This included building out the buttons, front-end routes, and user input database manipulation. It was very exciting working with a team, and confronting all of the challenges that come with it.",
+    image: "./portfolio.png",
+    link: "https://booknborrow.herokuapp.com/",
+    github: "https://github.com/joaqsala/book-n-borrow",
+    technologies:
+      "Handlebars, Bootstrap, Nodejs, Express, MySQL, Sequelize, Heroku, Figma, Jira",
+  },
+  {
+    id: 5,
+    title: "Ecommerce Backend",
+    description:
+      "This project was built to showcase my skills in building a backend for an ecommerce website. I used MySQL and Sequelize to build the database and models. I used Express to build the routes and Nodejs to run the server. I used Postman to test the routes and make sure that they were working properly. I also used Dotenv to hide my sensitive information. It is very exciting to learn the inner workings of a relational database, and how these skills could be used on a much larger scale.",
+    image: "./portfolio.png",
+    link: "https://github.com/KodaCaleb/ecommerce",
+    github: "https://github.com/KodaCaleb/ecommerce",
+    technologies: "Nodejs, Express, MySQL, Sequelize, Dotenv, Postman",
+  },
+  {
+    id: 6,
+    title: "Book Search Engine",
+    description:
+      "This project is emblematic of the fact that most modern websites are driven by two things: data, and user demands. The ability to personalize user data is the cornerstone of real-world web development, and I took on this project to learn how to deal with this challenge. While building this application I was faced with the challenges that come with manipulating data within a non-relational database (mongoDB using GraphQL), and learned how to overcome many of these challenges. I have found that I absolutely love using GraphQL and Apollo Server and am very excited to use these again in the future.",
+    image: "./portfolio.png",
+    link: "https://github.com/KodaCaleb",
+    github: "https://github.com/KodaCaleb",
+    technologies: "React, GraphQL, Apollo Server, MongoDB, Heroku",
+  },
+];
