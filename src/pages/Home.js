758ac16e2bf32705defc1bb78de6747ed9c9ee4f
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-29 05:04:55 -0500

    cleaned up app, ready to deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Hero from "../components/home/hero";
+import Quotes from "../components/home/quotes";
 
 export default function Home() {
     return (
-        <div className="w-screen h-full flex-col">
+        <div className="w-screen h-full flex flex-row">
             <Hero />
+            <Quotes />
         </div>
     );
 }
