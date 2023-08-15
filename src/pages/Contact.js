18aa6405906f2d27dacc1044188bc27d2901e82a
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-08-15 13:29:34 -0500

    edited type animation in contact page to prevent wrapping

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     return (
         <div className="w-full h-full flex flex-col sm:flex-row">
             <div className="sm:w-1/2 w-2/3 self-center h-auto  flex">
-                <div className="sm:self-center self-start w-full h-auto text-start sm:h-auto flex justify-center items-start text-3xl sm:text-5xl p-10 sm:p-20 text-themeWhite font-extralight ">
+                <div className="sm:self-center self-start w-full h-40 text-start sm:h-auto flex justify-center items-start text-3xl sm:text-5xl p-10 sm:p-20 text-themeWhite font-extralight ">
                     <TypeAnimation
                     sequence={["Let's build something great together."]}
                     speed={50}
